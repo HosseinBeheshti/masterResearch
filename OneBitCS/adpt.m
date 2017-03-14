775f1a7041753e467be7b8b5cb6d878a5ade32cd
HosseinBeheshti <h.beheshti92@gmail.com>
2017-03-14 18:13:15 +0330

    new adaptive temp not valid respond

--- OneBitCS/adpt.m
+++ OneBitCS/adpt.m
@@ -1,21 +1,23 @@
 function z_adpt = adpt(x_temp,n,m,step)
 % measure procedure
-    delta_r = zeros(m,1);
-    A   = zeros(m,n);
+    A   = randn(m,n);
     tau = zeros(m,1);
+    Phi = zeros(n,m);
     y   = zeros(m,1);
-    A(1,:)  = randn(1,n);
-    tau(1,1)  = randn(1,1);
-    y(1,1)    = sign(A(1,:)*x_temp-tau(1));
-    for i=1:m-1
-        if (y(i)>= 0 && tau(i)>= 0)||(y(i)<0 && tau(i)<0)
-            delta_r(i+1) = delta_r(i)+step;
-        else
-            delta_r(i+1) = delta_r(i)-step;
-        end
-        A(i+1,:)  = randn(1,n);
-        tau(i+1)  = randn(1)+delta_r(i+1);  
-        y(i+1,1)    = sign(A(i+1,:)*x_temp-tau(i+1));
+    y(1)    = sign(A(1,:)*x_temp+A(1,:)*Phi(:,1)-tau(1));
+    y(2)    = sign(A(2,:)*x_temp+A(2,:)*Phi(:,2)-tau(2));
+    for i=2:m-1
+    	Phi(:,i+1) = step.*(y(i).*A(i,:)'+y(i-1).*A(i-1,:)')/2;
+        y(i+1)    = sign(A(i+1,:)*x_temp+A(i+1,:)*Phi(:,i+1)-tau(i+1));
+    end
+% visiual adaptivity for n = 2
+    if n == 2
+        close all;
+        figure;
+        hold on;
+        plot(Phi(1,:),Phi(2,:),'*'); 
+        plot(x_temp(1),x_temp(2),'o'); 
+        hold off;
     end
 % recovery procedure
     z_adpt = 1;
--- OneBitCS/main.m
+++ OneBitCS/main.m
@@ -5,11 +5,12 @@ tic;
 %% signal parameter
 n   = 2;     % signal dimension
 s   = 2;       % sparsity
-m   = 10;       % number of measurment
+m   = 100;       % number of measurment
+R   = 100;
 %% Generating a s-sparse signal in R^n
 x_temp = zeros(n,1);
 rp = randperm(n);
-x_temp(rp(1:s)) = randn(s,1); 
+x_temp(rp(1:s)) = nthroot(R, n).*randn(s,1); 
 r = norm(x_temp);
 x0 = [x_temp ; -1];
 %% Gaussian sensing matrix and associated 1-bit sensing
@@ -22,7 +23,7 @@ z_l1 = cvx1(y,n,s,m,N);
 %% BIHT 
 z_biht = BIHT(y,n,s,m,N);
 %% Adaptive algorithm 1
-step    = 1;
+step    = 2;
 z_adpt = adpt(x_temp,n,m,step);
 %% IGP
 x_biht = z_biht.*(sqrt(r^2+1));
