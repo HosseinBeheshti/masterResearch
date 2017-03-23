6977663a428cc2558c7b907eb3b76323a6a1dda1
HosseinBeheshti <h.beheshti92@gmail.com>
2017-03-23 21:17:20 +0430

    start reconstruction

--- OneBitCS/adpt.m
+++ OneBitCS/adpt.m
@@ -1,14 +1,14 @@
 function z_adpt = adpt(x_temp,n,m,step,R)
 % measure procedure
-    A   = randn(m,n);
-    Phi = zeros(n,m);
-    d   = zeros(n,m);
-    y   = zeros(m,1);
+    A       = randn(m,n);
+    Phi     = zeros(n,m);
+    drct    = zeros(n,m);
+    y       = zeros(m,1);
     y(1)    = sign(A(1,:)*x_temp-A(1,:)*Phi(:,1));
     for i=1:m-1
-        d(:,i) = y(i).*A(i,:)';
-    	Phi(:,i+1) = Phi(:,i)+step.*(d(:,i))/norm(d(:,i));
-        y(i+1)    = sign(A(i+1,:)*x_temp-A(i+1,:)*Phi(:,i+1));
+        drct(:,i)   = y(i).*A(i,:)';
+    	Phi(:,i+1)  = Phi(:,i)+step.*(drct(:,i))/norm(drct(:,i));
+        y(i+1)      = sign(A(i+1,:)*x_temp-A(i+1,:)*Phi(:,i+1));
     end
 % visiual adaptivity for n = 2
     if n == 2 
@@ -21,12 +21,11 @@ function z_adpt = adpt(x_temp,n,m,step,R)
             pause(1)
             plot(t1,t2);
             plot(Phi(1,i),Phi(2,i),'*'); 
-            xlim([-2*nthroot(R, n) 2*nthroot(R, n)])
-            ylim([-2*nthroot(R, n) 2*nthroot(R, n)])
+            xlim([-4*nthroot(R, n) 4*nthroot(R, n)])
+            ylim([-4*nthroot(R, n) 4*nthroot(R, n)])
         end 
         hold off;
     end
-    
 % recovery procedure
     z_adpt = 1;
 end
