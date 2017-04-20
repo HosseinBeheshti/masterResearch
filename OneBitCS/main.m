222c413fb5c5f31273ce445d026e2f524a10db5b
HosseinBeheshti <h.beheshti92@gmail.com>
2017-04-20 19:04:13 +0430

    some change

--- OneBitCS/main.m
+++ OneBitCS/main.m
@@ -20,54 +20,37 @@ if cs_m_en
 else
     m            	= 1000;
 end
-Rmax = 1; %upper bound for ||x||
-Rmin = .1; %lower bound for ||x||
+Rmax = 20; %upper bound for ||x||
+Rmin = 10; %lower bound for ||x||
 
 tau =Rmin% Threshold parameter: an alternative is 
 tau2 = Rmin/2+Rmax/2 %(works better with convex minimizaiton);
 %% signal generator
 x_org               = signal_generator(n, s, dtr_x, Rmin, Rmax);
 %% convex optimization Gpv
-x_Gpv               = Gpv(x_org,n,s,m,plot_Gpv);
+% x_Gpv               = Gpv(x_org,n,s,m,plot_Gpv);
+%% GBIHT 
+% x_Gbiht             = GBIHT(x_org,n,s,m,plot_GBIHT);
 %% convex optimization Apv
 x_Apv               = Apv(x_org,n,s,m,plot_Apv);
-%% GBIHT 
-x_Gbiht             = GBIHT(x_org,n,s,m,plot_GBIHT);
 %% AdptOneBitCS algorithm 1
-x_adpt              = AdptOneBitCS(x_org,n,s,m,plot_adpt);
+x_AdptOneBitCS   	= AdptOneBitCS(x_org,n,s,m,plot_adpt);
 %% 
-% [ xhatPV, xsharpPV, normxEstPV, xhatAlt, xsharpAlt, normxEstAlt, normxEstEDF ] = KarinKnudson(x_org, n, s, m, tau, tau2);
-%% plot result
-if o_plot
-figure
-subplot(2,1,1);
-hold on;
-stem(x_Gpv,'s');
-stem(x_Gbiht,'o');
-stem(x_adpt,'*');
-stem(x_org,'d');
-legend('x_{Gpv}','x_{Gbiht}','x_{adpt}','x0');
-hold off;
+[ xhatPV, xsharpPV, normxEstPV, xhatAlt, xsharpAlt, normxEstAlt, normxEstEDF ] = KarinKnudson(x_org, n, s, m, tau, tau2);
 
-subplot(2,1,2);
-hold on;
-stem(x_org-x_Gpv,'s');
-stem(x_org-x_Gbiht,'o');
-stem(x_org-x_adpt,'*');
-legend('x_{pv} err','x_{biht} err','x_{AdptOneBitCS} err');
-hold off;
-end
 %% norm 
 
 
-Gpv_err         = r_err_c(x_Gpv,x_org);
-Apv_err       = r_err_c(x_Apv,x_org);
-Gbiht_err       = r_err_c(x_Gbiht,x_org);
-adpt_err        = r_err_c(x_adpt,x_org);
+% Gpv_err         = r_err_c(x_Gpv,x_org);
+% Gbiht_err       = r_err_c(x_Gbiht,x_org);
+Apv_err         = r_err_c(x_Apv,x_org);
+bb_err         = r_err_c(xsharpPV,x_org);
+aa_err         = r_err_c(xsharpAlt,x_org);
+adpt_err        = r_err_c(x_AdptOneBitCS,x_org);
 
-disp(['Gpv_err = ',num2str(Gpv_err)])
+% disp(['Gpv_err = ',num2str(Gpv_err)])
+% disp(['Gbiht_err = ',num2str(Gbiht_err)])
 disp(['Apv_err = ',num2str(Apv_err)])
-disp(['Gbiht_err = ',num2str(Gbiht_err)])
 disp(['adpt_err = ',num2str(adpt_err)])
 
 disp(m)
