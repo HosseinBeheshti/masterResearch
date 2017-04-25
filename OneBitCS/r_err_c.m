d92cdbdae2ece9b9b8f28ca064c382875c88f369
HosseinBeheshti <h.beheshti92@gmail.com>
2017-04-25 21:10:30 +0430

    change error function to real signal not norm

--- OneBitCS/r_err_c.m
+++ OneBitCS/r_err_c.m
@@ -1,6 +1,6 @@
 function r_err = r_err_c(x,x_org)
 r_err   = zeros(1,size(x_org,2));
 for i=1:size(x_org,2)
-    r_err(i) = abs(norm(x(:,i))-norm(x_org(:,i)))/norm(x_org(:,i));
+    r_err(i) = norm(x(:,i)-x_org(:,i))/norm(x_org(:,i));
 end
 end
