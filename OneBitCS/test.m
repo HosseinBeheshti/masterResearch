f1259590ee9a12f7fad14305b5f0d3603dada2f5
HosseinBeheshti <h.beheshti92@gmail.com>
2017-05-06 17:40:04 +0430

    add test function current function mean width

---
+++ OneBitCS/test.m
@@ -0,0 +1,26 @@
+clear;
+close all;
+clc;
+tic;
+%% parameter
+n = 2;
+itr = 1000;
+w_cvx       = zeros(1,itr);
+%%
+for i = 1:itr
+    g = normrnd(0,1,1,n);
+    cvx_begin quiet;
+    variable z_cvx(n);
+    maximize g*z_cvx;
+    subject to
+    norm(z_cvx)     <= 50;
+    cvx_end
+    w_cvx(i)    = abs(g*z_cvx);
+end
+%%
+for i = 1:itr
+W_k(i) = sum(w_cvx(1:i))./length(w_cvx(1:i));
+end
+plot(W_k);
+%%
+n = toc;
