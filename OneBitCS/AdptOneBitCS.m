2484bdcf3d0eed6116a0bc6b8c60a909f5e9cfcb
HosseinBeheshti <h.beheshti92@gmail.com>
2017-05-06 17:00:53 +0430

    add mean width cvx

--- OneBitCS/AdptOneBitCS.m
+++ OneBitCS/AdptOneBitCS.m
@@ -3,6 +3,7 @@ blk_s       = 10;
 stage       = ceil(m/blk_s);
 A_var       = 1;
 Phi_var     = sqrt(Rmax).*ones(stage,1);
+w_cvx       = zeros(1,stage);
 A           = zeros(blk_s,n,stage);
 Phi         = zeros(n,blk_s,stage);
 ofset       = zeros(n,stage);
@@ -24,30 +25,44 @@ for i = 1:stage
     variable x_cvx(n);
     minimize(norm(x_cvx,1));
     subject to
-    for j=1:blk_s
-        if y(j,:,i)>=0
-            A(j,:,i)*x_cvx >= tau(j,:,i);
-        else
-            A(j,:,i)*x_cvx <= tau(j,:,i);
+    for k = 1:i
+        for j=1:blk_s
+            if y(j,:,k)>=0
+                A(j,:,k)*x_cvx >= tau(j,:,k);
+            else
+                A(j,:,k)*x_cvx <= tau(j,:,k);
+            end
         end
     end
     norm(x_cvx)     <= Rmax;
     cvx_end
     
     % Computing Gaussian width
+    g = normrnd(0,1,1,n);
     cvx_begin quiet;
-    variable w_cvx;
-    minimize w_cvx;
+    variable z_cvx(n);
+    maximize g*z_cvx;
     subject to
-    w_cvx
+    for k = 1:i
+        for j=1:blk_s
+            if y(j,:,k)>=0
+                A(j,:,k)*x_cvx >= tau(j,:,k);
+            else
+                A(j,:,k)*x_cvx <= tau(j,:,k);
+            end
+        end
+    end
+    norm(z_cvx)     <= Rmax;
     cvx_end
+    
+    w_cvx(i)    = abs(g*z_cvx);
     
     % set parameter
     if i==stage(end)
         x_adpt          = x_cvx;
     else
         ofset(:,i+1)   	= x_cvx;
-        Phi_var         = Phi_var;
+%         Phi_var(i+1)    = w_cvx(i);
     end
     
     %% visiual adaptivity
@@ -61,7 +76,7 @@ for i = 1:stage
             t1 = -4*nrm_inf:0.1:4*nrm_inf;
             for j =1:blk_s
                 t2 = -((A(j,1,i)/A(j,2,i))*(t1-Phi(1,j,i)))+Phi(2,j,i);
-                pause(0.5)
+%                 pause(0.5)
                 plot(t1,t2);
                 xlim([-2*nrm_inf 2*nrm_inf]);
                 ylim([-2*nrm_inf 2*nrm_inf]);
@@ -69,7 +84,7 @@ for i = 1:stage
             plot(x_org(1),x_org(2),'.r','markersize',40);
             pause(1)
             plot(x_cvx(1),x_cvx(2),'.b','markersize',35);
-            pause(2)
+            pause(1)
             hold off;
         end
     end
