2bf115d35c4ee57764d68b6b2369d41127cafe3b
HosseinBeheshti <h.beheshti92@gmail.com>
2017-05-18 20:05:04 +0430

    change to log barrier opt problem

--- OneBitCS/test.m
+++ OneBitCS/test.m
@@ -1,35 +1,24 @@
-clear;
-close all;
-clc;
-tic;
-%% parameter
-n = 2;
-s = 2;
-m = 100;
-itr = 200;
-Rmax    = 90; %upper bound for ||x||
-Rmin    = 70; %lower bound for ||x||
-%%
-A       = normrnd(0,1,m,n);
-tau     = normrnd(0,1,m,1);
-w_cvx   = zeros(1,itr);
-[x_org, trueNorm]   = signal_generator(n, s, 0, Rmin, Rmax);
-y       = theta(A*x_org-tau);
-for i = 1:itr
-    g = normrnd(0,1,1,n);
-    cvx_begin quiet;
-    variable z_cvx(n);
-    maximize g*z_cvx;
-    subject to
-    y.*(A*z_cvx-tau) >= 0;
-    norm(z_cvx)     <= Rmax;
-    cvx_end
-    w_cvx(i)    = abs(g*z_cvx)./norm(g);
-end
-%%
-for i = 1:itr
-    W_k(i) = sum(w_cvx(1:i))./length(w_cvx(1:i));
-end
-plot(W_k);
-%%
-n = toc
+clear, clc, close all
+
+%Dimension of the problem
+n = 2;  
+
+%Number of summation terms
+m = 20;
+
+%Random vector c
+c = 0*rand(n, 1);
+
+%Random vector b
+b = rand(m, 1);
+
+%Random array A
+A = rand(m, n);
+
+%CVX optimization
+cvx_begin
+    variable x(n)
+    minimize(c.'*x - sum(log(b - A*x)))
+    subject to      %Initially without this line
+        b - A*x > 0 %Initially without this line
+cvx_end
