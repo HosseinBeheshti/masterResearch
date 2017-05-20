cd0fd5f0622991cc6be034a7e7ba6774ae904aac
HosseinBeheshti <h.beheshti92@gmail.com>
2017-05-20 18:57:17 +0430

    Analytic center simple example

--- OneBitCS/test.m
+++ OneBitCS/test.m
@@ -8,24 +8,31 @@
 %           minimize    -sum_{i=1}^m log(b_i-a_i^Tx).
 
 % Input data
-randn('state', 0);
-rand('state', 0);
 n = 2;
-m = 50;
-p = 5;
-tmp = randn(n,1);
-A = randn(m,n);
-b = A*tmp + 10*rand(m,1);
-F = randn(p,n);
-g = F*tmp;
+m = 4;
+L_inf = 10;
 
+A = [-1,eps;1,eps;eps,-1;eps,1];
+b = [1;2;1;2];
 % Analytic center
 cvx_begin
     variable x(n)
     minimize -sum(log(b-A*x))
-    F*x == g
 cvx_end
 
 disp(['The analytic center of the set of linear inequalities and ' ...
       'equalities is: ']);
 disp(x);
+%%
+t1 = -4*L_inf:(L_inf/100):4*L_inf;
+close all;
+figure(1)
+hold on;
+for j =1:m
+    t2 = -(A(j,1)/A(j,2))*t1+(b(j)/A(j,2));
+    plot(t1,t2);
+    xlim([-L_inf L_inf]);
+    ylim([-L_inf L_inf]);
+end
+plot(x(1),x(2),'.g','markersize',40);
+hold off;
