8b107c9889e45fa53c3b32acd1612d2a204e8d59
HosseinBeheshti <h.beheshti92@gmail.com>
2017-05-21 23:27:07 +0430

    Chebyshev center example

--- OneBitCS/test.m
+++ OneBitCS/test.m
@@ -6,37 +6,45 @@
 % radius) that lies in a polyhedron described by linear inequalites in this
 % fashion: P = {x : a_i'*x <= b_i, i=1,...,m} where x is in R^2
 
-% Generate the input data
-a1 = [ 2;  1];
-a2 = [ 2; -1];
-a3 = [-1;  2];
-a4 = [-1; -2];
-b = ones(4,1);
+load log;
 
-% Create and solve the model
-cvx_begin
-    variable r(1)
-    variable x_c(2)
-    maximize ( r )
-    a1'*x_c + r*norm(a1,2) <= b(1);
-    a2'*x_c + r*norm(a2,2) <= b(2);
-    a3'*x_c + r*norm(a3,2) <= b(3);
-    a4'*x_c + r*norm(a4,2) <= b(4);
+% Computing Chebyshev center
+cvx_begin quiet;
+variable r(1)
+variable x_c(2)
+maximize (r)
+for k=1:blk_s
+(-y(k,:,i).*A(k,:,i))*x_c +r*norm(-y(k,:,i).*A(k,:,i),2) <= -y(k,:,i).*tau(k,:,i);
+end
 cvx_end
+%%
+close all;
+figure(1);
+hold on;
+% norm constraint
+sp  = -L_inf:(L_inf/100):L_inf;
+plot(L_inf*ones(length(sp)),sp,'.b','markersize',5);
+plot(-L_inf*ones(length(sp)),sp,'.b','markersize',5);
+plot(sp,L_inf*ones(length(sp)),'.b','markersize',5);
+plot(sp,-L_inf*ones(length(sp)),'.b','markersize',5);
+plot(x_org(1),x_org(2),'.r','markersize',40);
+t1 = -4*L_inf:(L_inf/100):4*L_inf;
+for k = 1:i
+    for j =1:blk_s
+        t2 = -((A(j,1,k)/A(j,2,k))*(t1-Phi(1,j,k)))+Phi(2,j,k);
+        plot(t1,t2);
+%         pause(3);
+        xlim([-L_inf L_inf]);
+        ylim([-L_inf L_inf]);
+    end
+end
+% x and xhat
+plot(x_c(1),x_c(2),'.g','markersize',40);
+
 
 % Generate the figure
 x = linspace(-2,2);
 theta = 0:pi/100:2*pi;
-plot( x, -x*a1(1)./a1(2) + b(1)./a1(2),'b-');
-hold on
-plot( x, -x*a2(1)./a2(2) + b(2)./a2(2),'b-');
-plot( x, -x*a3(1)./a3(2) + b(3)./a3(2),'b-');
-plot( x, -x*a4(1)./a4(2) + b(4)./a4(2),'b-');
 plot( x_c(1) + r*cos(theta), x_c(2) + r*sin(theta), 'r');
 plot(x_c(1),x_c(2),'k+')
-xlabel('x_1')
-ylabel('x_2')
-title('Largest Euclidean ball lying in a 2D polyhedron');
-axis([-1 1 -1 1])
-axis equal
- 
+hold off;
