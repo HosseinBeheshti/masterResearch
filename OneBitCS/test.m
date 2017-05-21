b8e0a2ce3e36071fb4b881f9d118db7539cc8cf2
HosseinBeheshti <h.beheshti92@gmail.com>
2017-05-21 20:57:17 +0430

    Section 4.3.1: Compute and display the Chebyshev center of a 2D polyhedron

--- OneBitCS/test.m
+++ OneBitCS/test.m
@@ -1,55 +1,42 @@
-clear;
-close all;
-clc;
-tic;
-%% parameter
-n = 2;
-s = 2;
-m = 15;
-itr = 10;
-Rmax    = 5; %upper bound for ||x||
-Rmin    = 1; %lower bound for ||x||
-%%
-A       = normrnd(0,1,m,n);
-tau     = 3*normrnd(0,1,m,1);
-[x_org, trueNorm]   = signal_generator(n, s, 0, Rmin, Rmax);
-y       = theta(A*x_org-tau);
+% Boyd & Vandenberghe, "Convex Optimization"
+% Joëlle Skaf - 08/16/05
+% (a figure is generated)
+%
+% The goal is to find the largest Euclidean ball (i.e. its center and
+% radius) that lies in a polyhedron described by linear inequalites in this
+% fashion: P = {x : a_i'*x <= b_i, i=1,...,m} where x is in R^2
 
-g = normrnd(0,1,1,n);
-cvx_begin quiet;
-variable w_s(n);
-maximize g*w_s;
-subject to
-y.*(A*w_s-tau) >= 0;
-norm(w_s,inf)  <= Rmax;
+% Generate the input data
+a1 = [ 2;  1];
+a2 = [ 2; -1];
+a3 = [-1;  2];
+a4 = [-1; -2];
+b = ones(4,1);
+
+% Create and solve the model
+cvx_begin
+    variable r(1)
+    variable x_c(2)
+    maximize ( r )
+    a1'*x_c + r*norm(a1,2) <= b(1);
+    a2'*x_c + r*norm(a2,2) <= b(2);
+    a3'*x_c + r*norm(a3,2) <= b(3);
+    a4'*x_c + r*norm(a4,2) <= b(4);
 cvx_end
 
-cvx_begin quiet;
-variable w_i(n);
-maximize -g*w_i;
-subject to
-y.*(A*w_i-tau) >= 0;
-norm(w_i,inf)  <= Rmax;
-cvx_end
-
-w_cvx    = abs(w_s-w_i);
-
-%%
-L_inf = 10;
-t1 = -L_inf:(L_inf/100):L_inf;
-close all;
-figure(1)
-hold on;
-for j =1:m
-    t2 = -(A(j,1)/A(j,2))*t1+(tau(j)/A(j,2));
-    plot(t1,t2);
-    xlim([-L_inf L_inf]);
-    ylim([-L_inf L_inf]);
-end
-plotv(g');
-plot(x_org(1),x_org(2),'.r','markersize',40);
-plot(w_s(1),w_s(2),'.b','markersize',40);
-plot(w_i(1),w_i(2),'.g','markersize',40);
-hold off;
-%%
-n = toc
+% Generate the figure
+x = linspace(-2,2);
+theta = 0:pi/100:2*pi;
+plot( x, -x*a1(1)./a1(2) + b(1)./a1(2),'b-');
+hold on
+plot( x, -x*a2(1)./a2(2) + b(2)./a2(2),'b-');
+plot( x, -x*a3(1)./a3(2) + b(3)./a3(2),'b-');
+plot( x, -x*a4(1)./a4(2) + b(4)./a4(2),'b-');
+plot( x_c(1) + r*cos(theta), x_c(2) + r*sin(theta), 'r');
+plot(x_c(1),x_c(2),'k+')
+xlabel('x_1')
+ylabel('x_2')
+title('Largest Euclidean ball lying in a 2D polyhedron');
+axis([-1 1 -1 1])
+axis equal
+ 
