76f6cc6354f0b2d6b5981ef30886cc0ab630faed
HosseinBeheshti <h.beheshti92@gmail.com>
2017-05-21 08:44:31 +0430

    change to sup inf mean width

--- OneBitCS/test.m
+++ OneBitCS/test.m
@@ -1,34 +1,42 @@
-
-%% signal parameter
-n                   = 2; % signal dimension
-s                   = 2; % sparsity
-% number of measurment
-m_temp           	= 120;
-
-Rmax    = 2; % upper bound for ||x||
-Rmin    = 1; % lower bound for ||x||
-L_inf   = 10; % upper bound of ||x||_{\inf}
-A_var       = 1;
+clear;
+close all;
+clc;
+tic;
+%% parameter
 n = 2;
-m = 10;
-
-[x_org,~]  = signal_generator(n, s, 0, Rmin, Rmax);
-
+s = 2;
+m = 15;
+itr = 10;
+Rmax    = 5; %upper bound for ||x||
+Rmin    = 1; %lower bound for ||x||
+%%
 A       = normrnd(0,1,m,n);
-tau     = 5*normrnd(0,1,m,1);
-yp      = A*x_org-tau;
-y       = theta(yp);
+tau     = 3*normrnd(0,1,m,1);
+[x_org, trueNorm]   = signal_generator(n, s, 0, Rmin, Rmax);
+y       = theta(A*x_org-tau);
 
-
-% Analytic center
-cvx_begin
-variable x(n)
-minimize -sum(log(y.*(A*x-tau)));
+g = normrnd(0,1,1,n);
+cvx_begin quiet;
+variable w_s(n);
+maximize g*w_s;
+subject to
+y.*(A*w_s-tau) >= 0;
+norm(w_s,inf)  <= Rmax;
 cvx_end
 
-disp(x);
+cvx_begin quiet;
+variable w_i(n);
+maximize -g*w_i;
+subject to
+y.*(A*w_i-tau) >= 0;
+norm(w_i,inf)  <= Rmax;
+cvx_end
+
+w_cvx    = abs(w_s-w_i);
+
 %%
-t1 = -5*L_inf:(L_inf/100):5*L_inf;
+L_inf = 10;
+t1 = -L_inf:(L_inf/100):L_inf;
 close all;
 figure(1)
 hold on;
@@ -38,6 +46,10 @@ for j =1:m
     xlim([-L_inf L_inf]);
     ylim([-L_inf L_inf]);
 end
+plotv(g');
 plot(x_org(1),x_org(2),'.r','markersize',40);
-plot(x(1),x(2),'.g','markersize',40);
+plot(w_s(1),w_s(2),'.b','markersize',40);
+plot(w_i(1),w_i(2),'.g','markersize',40);
 hold off;
+%%
+n = toc
