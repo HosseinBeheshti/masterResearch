880785948ef693ac9cb9e729db00f7d630152589
HosseinBeheshti <h.beheshti92@gmail.com>
2017-07-04 17:31:11 +0430

    some problem in MPT3 package

--- OneBitCS/test.m
+++ OneBitCS/test.m
@@ -2,28 +2,37 @@
 %%
 close all;
 clc;
+load log
 %%
 g = x_ac'./norm(x_ac);
-L = 0.1;
+
+L = 0.01;
+
 ply_volume = zeros(1,10);
 hold on;
-plot(0,0)
-plot(x_ac(1),x_ac(2),'.g','markersize',40);
+
+plot(x_ac(1),x_ac(2),'.y','markersize',40);
 plot(current_Polyhedron,'color','red','alpha',0.2)
+
 for i = 1:10
+    a = x_ac-L.*g';
+    b = x_ac+L.*g';
+    hyp1 = norm(a);
+    hyp2 = norm(b);
+    n1 = a./hyp1;
+    n2 = b./hyp2;
     
-    hyp1 = norm(x_ac)+L;
-    hyp2 = norm(x_ac)-L;
-    
-    ply_nrml_1 = [ply_nrml ; g ;-g];
-    ply_ofst_1 = [ply_ofst ; hyp1 ;hyp2];
+    ply_nrml_1 = [ply_nrml ; n1'];
+    ply_ofst_1 = [ply_ofst ; hyp1];
     Polyhedron_v = Polyhedron(ply_nrml_1,ply_ofst_1);
     ply_volume(i) = volume(Polyhedron_v);
-    
+    plot(a(1),a(2),'.g','markersize',20);
+    plot(b(1),b(2),'.b','markersize',20);
     plot(Polyhedron_v,'color','blue','alpha',0.2)
+
     pause(0.5)
     
-    L = L+0.1;
+    L = L+0.01;
 end
 hold off;
 %%
