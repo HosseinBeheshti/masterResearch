5f5ec1a51d12c8f679b9ce8df8e6d253b7fe173b
HosseinBeheshti <h.beheshti92@gmail.com>
2017-07-13 21:42:28 +0430

    some change in plot function

--- test.m
+++ test.m
@@ -6,6 +6,7 @@ load log;
 %%
 % % test_poly = slice
 %%
+if 0
 close all;
 sweep_d = -L_inf:2:L_inf;
 Volume_clt = zeros(n,length(sweep_d));
@@ -14,8 +15,6 @@ if n==2 || n==3
     figure(1)
     plot(current_Polyhedron,'color','blue','alpha',0.1)
 end
-figure(2)
-hold on;
 for i = 1:n
     for k =1:length(sweep_d)
         slice_poly = slice(current_Polyhedron,i,sweep_d(k),'keepDim',true);
@@ -24,11 +23,15 @@ for i = 1:n
         Volume_clt(i,k) = volume(F);
     end
     disp(i/n)
+end
+volume(current_Polyhedron)
+toc
+save log;
+end
+%%
+figure(2)
+hold on;
+for i = 1:n
     plot(Volume_clt(i,:))
 end
 hold off;
-
-volume(current_Polyhedron)
-toc
-
-save log;
