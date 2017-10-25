d5da882ce0d0f7b641b37dd40fa7ee0930af746f
HosseinBeheshti <h.beheshti92@gmail.com>
2017-10-25 12:17:55 +0330

    LP main completed

--- main.m
+++ main.m
@@ -2,29 +2,33 @@ clear;
 clc;                     % clear the workspace and the screen 
 %%
 % define the sparse vector x 
-N = 100;                      	% size of x
-s = 10;                         % sparsity of x
+N = 10;                      	% size of x
+s = 1;                         % sparsity of x
 supp = sort(randsample(N,s));   % support of x 
 x = zeros(N,1); 
 x(supp) = randn(s,1);       	% entries of x on its support
 
 % Generate dictionary
-n = 10;                        % number of dictionary rows
+n = 2;                        % number of dictionary rows
 DType = 'Rl';                   % dictionary type /in {ODFT}
 D = DictionaryGenerator(n,N,DType);
 f = D*x;
-a = D*D';
+
 r = 1.5*norm(f);                % an (over)estimation of the magnitude of f
 
 %%
 % specify the random measurements to be used
-m = 10000;                      % number of measurements
-A = randn(m,n);                 % matrix whose rows give the measurements
+m = 100;                      % number of measurements
+A = randn(m,n);              % measurement matrix 
 
-T = 8;                    % number of adaptive groups of measurements
-fLP_main = LP_main(x,A,s,r,T);
+%% LP main
+DitherType = 'LP';
+sigma = r;
+tau = DitherGenerator(m,sigma,DitherType);
 
-
+y = sign(A*f-tau);
+fLP_main = LP_main(y,A,D,sigma,tau);
+%% Adaptive LP
 
 
 
