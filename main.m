fdca915b2b97e5bea019565e6184691ced0eeae1
HosseinBeheshti <h.beheshti92@gmail.com>
2017-11-27 21:23:57 +0330

    temp for sign(af)=y

--- main.m
+++ main.m
@@ -22,29 +22,31 @@ x = zeros(N,1);
 x(supp) = randn(s,1);       	% entries of x on its support
 
 % Generate dictionary
-n = 50;                        % number of dictionary rows
+n = 900;                        % number of dictionary rows
 DType = 'Rl';                   % dictionary type /in {ODFT}
 D = DictionaryGenerator(n,N,DType);
 f = D*x;
-kapa = nnz(D'*D*x)/nnz(x);
+kapa = numel(find(abs(D'*f)>0.1))/s;
 r = 2*norm(f);                % an (over)estimation of the magnitude of f
 
 %%
 % specify the random measurements to be used
-m = 50000;                      % number of measurements
+m = 500;                      % number of measurements
 A = randn(m,n);              % measurement matrix
 %% CP
-fCP_main = CP_main(D,A,f,r,r);
-err_CP = norm(fCP_main-f)/norm(f);
+% fCP_main = CP_main(D,A,f,r,r);
+% err_CP = norm(fCP_main-f)/norm(f);
 
 %% Adaptive CP
 T = 10; % number of batch
-fACP_main = ACP_main(D,A,f,r,r,T);
+% fACP_main = ACP_main(D,A,f,r,r,T);
 
 %% HT
-HT_eps = r.^2/();
-HT_epsPrim = r.^2/();
-t_HT = ceil(16()*kapa*(s+1));
+HT_sigma = r;
+
+HT_eps = 1;
+HT_epsPrim = ((r*HT_sigma)/(2*(r^2+HT_sigma^2)))*HT_eps;
+t_HT = ceil(16*((HT_epsPrim/8)^(-2))*kapa*(s+1));
 fHT_main = HT_main(D,A,f,t_HT,r,x);
 err_HT = norm(fHT_main-f)/norm(f);
 
