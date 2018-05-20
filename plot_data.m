60deecc1eb87d5e2beb563f7a3e1d1e2a522484b
HosseinBeheshti <h.beheshti92@gmail.com>
2018-05-20 08:55:36 +0430

    SimFig2 generated

--- plot_data.m
+++ plot_data.m
@@ -2,8 +2,8 @@ clear;
 close all;
 clc;
 %% load data
-Error_CP_Tt = zeros(3,200);
-Error_ACP_Tt = zeros(3,200);
+Error_CP_Tt = zeros(5,200);
+Error_ACP_Tt = zeros(5,200);
 load('SimResult_N=1000_n=50_s=10');
 Error_CP_Tt(1,:) = Error_CP_T;
 Error_ACP_Tt(1,:) = Error_ACP_T;
@@ -13,23 +13,28 @@ Error_ACP_Tt(2,:) = Error_ACP_T;
 load('SimResult_N=1000_n=50_s=30');
 Error_CP_Tt(3,:) = Error_CP_T;
 Error_ACP_Tt(3,:) = Error_ACP_T;
+load('SimResult_N=1000_n=50_s=40');
+Error_CP_Tt(4,:) = Error_CP_T;
+Error_ACP_Tt(4,:) = Error_ACP_T;
+load('SimResult_N=1000_n=50_s=50');
+Error_CP_Tt(5,:) = Error_CP_T;
+Error_ACP_Tt(5,:) = Error_ACP_T;
 %% plot result
 close all;
 hold all;
-legendInfo = cell(1,6);
-for s_it=1:3
-    plot(((0:(T_it_number-1))*Step_m+Min_m),10*log10(Error_CP_Tt(s_it,:)));
-    legendInfo{s_it} = ['CP s = ' num2str((s_it)*10)];
-end
+plot(100:1000:20000,10*log10(Error_ACP_Tt(1,1:10:end)),'DisplayName','s=10','Marker','o','LineStyle','-.',...
+    'Color',[0 0.5 0],'LineWidth',1.5);
+plot(100:1000:20000,10*log10(Error_ACP_Tt(2,1:10:end)),'DisplayName','s=20','Marker','*','LineStyle','--',...
+    'Color',[1 0 0],'LineWidth',1.5);
+plot(100:1000:20000,10*log10(Error_ACP_Tt(3,1:10:end)),'DisplayName','s=30','Marker','^','LineStyle',':',...
+    'Color',[0 1 0],'LineWidth',1.5);
+plot(100:1000:20000,10*log10(Error_ACP_Tt(4,1:10:end)),'DisplayName','s=40','Marker','diamond','LineStyle','-',...
+    'Color',[0 0 1],'LineWidth',1.5);
+plot(100:1000:20000,10*log10(Error_ACP_Tt(5,1:10:end)),'DisplayName','s=50','Marker','square','LineStyle','-.',...
+    'Color',[1 0 1],'LineWidth',1.5);
 
-for s_it=1:3
-    plot(((0:(T_it_number-1))*Step_m+Min_m),10*log10(Error_ACP_Tt(s_it,:)));
-    legendInfo{3+s_it} = ['ACP s = ' num2str((s_it)*10)];
-end
-
-legend(legendInfo)
-ylabel('Reconstruction Error (dB)')
-xlabel('measurments')
+ylabel('Nomalized reconstruction error (dB)')
+xlabel('Number of measurments')
 
 hold off;
 matlab2tikz('TikzFig.tex')
