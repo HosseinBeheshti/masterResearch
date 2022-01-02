d494562be89961418d837f6fe48e84c74d20e783
HosseinBeheshti <h.beheshti92@gmail.com>
2022-01-02 20:10:33 +0330

    add progress bar temp simulation result checked

--- main.m
+++ main.m
@@ -1,11 +1,13 @@
 clear;
 clc;
 close all;
-tic;
+%% Initialize progress bar:
+disp("start simulation");
+upd = textprogressbar(max_mcr);
 %% monte carlo
-max_mcr = 500;
+max_mcr = 100;
 %% number of measurements
-Max_m = 20000;
+Max_m = 25000;
 Step_m = 500;
 Min_m = 100;
 T_it_number = floor((Max_m-Min_m)/Step_m)+1;
@@ -21,8 +23,7 @@ N = 1000; % size of x
 s = 10; % sparsity of x
 n = 50; % number of dictionary rows
 T = 10; % number of batch
-%%
-disp("start simulation");
+%% monte_carlo loop
 for mcr = 1:max_mcr
     parfor itr_i=1:T_it_number
         %% Generate signal
@@ -30,38 +31,29 @@ for mcr = 1:max_mcr
         supp = sort(randsample(N,s));   % support of x
         x = zeros(N,1);
         x(supp) = randn(s,1);       	% entries of x on its support
-        
         % Generate dictionary
         D = DictionaryGenerator(n,N);
         f = D*x;
         r = 2*norm(f);                  % an (over)estimation of the magnitude of f
-        
         % specify the random measurements to be used
         m = (itr_i-1)*Step_m+Min_m;     % number of measurements
         A = randn(m,n);                 % measurement matrix
-        
         %% LP
         fLP_main = LP_main(D,A,f,r);
-        
         %% CP
         fCP_main = CP_main(D,A,f,r,r);
-        
         %% Adaptive CP
         fACP_main = ACP_main(D,A,f,r,T);
-        
         %% Compute error
         err_LP = norm(fLP_main-f)/norm(f);
         err_CP = norm(fCP_main-f)/norm(f);
         err_ACP = norm(fACP_main(:,end)-f)/norm(f);
-        
         Error_LP(itr_i) = err_LP;
         Error_CP(itr_i) = err_CP;
         Error_ACP(itr_i) = err_ACP;
-        
-        fprintf('monte carlo : %d\n',mcr)
-        fprintf('measurements: %d\n',itr_i)
     end
-    
+    % Update progress bar:
+    upd(mcr);
     FileName=[TempName,num2str(mcr)];
     save(FileName)
 end
@@ -80,10 +72,8 @@ end
 Error_LP_T = Error_LP_T./max_mcr;
 Error_CP_T = Error_CP_T./max_mcr;
 Error_ACP_T = Error_ACP_T./max_mcr;
-
 SimName=[SimFileName,'_N=',num2str(N),'_n=',num2str(n),'_s=',num2str(s),'_T=',num2str(T)];
-save(SimName)
-
+save(SimName);
 %% remove temporary file
 for mcr =1:max_mcr
     FileName=[TempName,num2str(mcr)];
@@ -91,7 +81,7 @@ for mcr =1:max_mcr
 end
 %% plot result
 close all;
-load('SimResult_N=1000_n=100_s=10_T=10');
+load(SimName);
 hold on;
 plot(((0:(T_it_number-1))*Step_m+Min_m),10*log10(Error_LP_T),'DisplayName','LP','Marker','*','LineStyle','-.',...
     'Color',[0 0.5 0],'LineWidth',1.5);
@@ -106,5 +96,4 @@ hold off;
 TikzName=['Tikz-',datestr(now, 'dd-mmm-yyyy'),'.tex'];
 matlab2tikz(TikzName)
 %%
-toc
 
