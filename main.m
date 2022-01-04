0825593a885ab98b64a65a65e951a1b8d92b0766
HosseinBeheshti <h.beheshti92@gmail.com>
2022-01-04 16:07:14 +0330

    remove opti add iteration time in saved data

--- main.m
+++ main.m
@@ -13,6 +13,9 @@ T_it_number = floor((Max_m-Min_m)/Step_m)+1;
 Error_LP = zeros(1,T_it_number);
 Error_CP = zeros(1,T_it_number);
 Error_ACP = zeros(1,T_it_number);
+time_LP = zeros(1,T_it_number);
+time_CP = zeros(1,T_it_number);
+time_ACP = zeros(1,T_it_number);
 %% file name
 TempName = 'TempFile_';
 SimFileName = 'SimResult';
@@ -24,6 +27,8 @@ T = 10; % number of batch
 %%
 disp("start simulation");
 for mcr = 1:max_mcr
+    fprintf('\n monte carlo : %d\n',mcr)
+    fprintf('measuerment :');
     parfor itr_i=1:T_it_number
         %% Generate signal
         % define the sparse vector x
@@ -41,27 +46,30 @@ for mcr = 1:max_mcr
         A = randn(m,n);                 % measurement matrix
         
         %% LP
+        tic;
         fLP_main = LP_main(D,A,f,r);
+        time_LP(itr_i) = toc;
         
         %% CP
+        tic;
         fCP_main = CP_main(D,A,f,r,r);
+        time_LP(itr_i) = toc;
         
         %% Adaptive CP
+        tic;
         fACP_main = ACP_main(D,A,f,r,T);
+        time_LP(itr_i) = toc;
         
         %% Compute error
         err_LP = norm(fLP_main-f)/norm(f);
         err_CP = norm(fCP_main-f)/norm(f);
         err_ACP = norm(fACP_main(:,end)-f)/norm(f);
-        
         Error_LP(itr_i) = err_LP;
         Error_CP(itr_i) = err_CP;
         Error_ACP(itr_i) = err_ACP;
         
-        fprintf('monte carlo : %d\n',mcr)
-        fprintf('measurements: %d\n',itr_i)
+        fprintf(' %d ',itr_i)
     end
-    
     FileName=[TempName,num2str(mcr)];
     save(FileName)
 end
@@ -70,16 +78,25 @@ end
 Error_LP_T = zeros(1,length(Error_LP));
 Error_CP_T = zeros(1,length(Error_CP));
 Error_ACP_T = zeros(1,length(Error_ACP));
+time_LP_T = zeros(1,length(Error_LP));
+time_CP_T = zeros(1,length(Error_CP));
+time_ACP_T = zeros(1,length(Error_ACP));
 for mcr =1:max_mcr
     FileName=[TempName,num2str(mcr)];
     load(FileName)
     Error_LP_T = Error_LP_T+Error_LP;
     Error_CP_T = Error_CP_T+Error_CP;
     Error_ACP_T = Error_ACP_T+Error_ACP;
+    time_LP_T = time_LP_T+Error_LP;
+    time_CP_T = time_CP_T+Error_CP;
+    time_ACP_T = time_ACP_T+Error_ACP;
 end
 Error_LP_T = Error_LP_T./max_mcr;
 Error_CP_T = Error_CP_T./max_mcr;
 Error_ACP_T = Error_ACP_T./max_mcr;
+time_LP_T = time_LP_T./max_mcr;
+time_CP_T = time_CP_T./max_mcr;
+time_ACP_T = time_ACP_T./max_mcr;
 
 SimName=[SimFileName,'_N=',num2str(N),'_n=',num2str(n),'_s=',num2str(s),'_T=',num2str(T),'_montecarlo_itr=',num2str(max_mcr)];
 save(SimName)
@@ -89,22 +106,7 @@ for mcr =1:max_mcr
     FileName=[TempName,num2str(mcr)];
     delete Temp*
 end
-%% plot result
-close all;
-load(SimName);
-hold on;
-plot(((0:(T_it_number-1))*Step_m+Min_m),10*log10(Error_LP_T),'DisplayName','LP','Marker','*','LineStyle','-.',...
-    'Color',[0 0.5 0],'LineWidth',1.5);
-plot(((0:(T_it_number-1))*Step_m+Min_m),10*log10(Error_CP_T),'DisplayName','CP','Marker','diamond','LineStyle','--',...
-    'Color',[1 0 0],'LineWidth',1.5);
-plot(((0:(T_it_number-1))*Step_m+Min_m),10*log10(Error_ACP_T),'DisplayName','Our algorithm','Marker','square',...
-    'Color',[0 0 1],'LineWidth',1.5);
-legend('LP','CP','Our algorithm')
-ylabel('Nomalized reconstruction error (dB)')
-xlabel('Number of measurments')
-hold off;
-TikzName=['Tikz-',datestr(now, 'dd-mmm-yyyy'),'.tex'];
-matlab2tikz(TikzName)
 %%
+disp("simulation finished");
 toc
 
