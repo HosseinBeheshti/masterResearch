919b697226b87600e7dfe652167005092d6ad43a
HosseinBeheshti <h.beheshti92@gmail.com>
2022-01-07 19:48:26 +0330

    simulate LP and CP when s = 20

--- main.m
+++ main.m
@@ -4,7 +4,7 @@ close all;
 %% monte carlo
 max_mcr = 100;
 %% number of measurements
-max_m = 40000;
+max_m = 30000;
 step_m = 1000;
 min_m = 1000;
 total_itr_number = floor((max_m - min_m) / step_m) + 1;
@@ -21,7 +21,7 @@ temp_name = 'temp_file_';
 N = 1000; % size of x
 n = 50; % number of dictionary rows
 T = 10; % number of batch
-s = [10 20 30]';
+s = 10; % sparsity level
 %%
 simulaiton_result.temp.total_itr_number = total_itr_number;
 simulaiton_result.temp.step_m = step_m;
@@ -33,80 +33,81 @@ simulaiton_result.temp.time_lp = zeros(1, total_itr_number);
 simulaiton_result.temp.time_cp = zeros(1, total_itr_number);
 simulaiton_result.temp.time_acp = zeros(1, total_itr_number);
 %% main loop
+disp('########################### H128B717 ###########################');
+disp('monte_carlo:');
 
-for sparsity_irt = 1:length(s)
-    disp('########################### H128B717 ###########################');
-    notif = ['start simulation with sparsity: ', num2str(s(sparsity_irt))];
-    disp(notif);
-    disp('monte_carlo:');
+for mcr = 1:max_mcr
+    itr_time = tic;
 
-    for mcr = 1:max_mcr
-        itr_time = tic;
-
-        parfor itr_i = 1:total_itr_number
-            %% Generate signal
-            % define the sparse vector x
-            supp = sort(randsample(N, s(sparsity_irt)));
-            x = zeros(N, 1);
-            x(supp) = randn(s(sparsity_irt), 1);
-            % Generate dictionary
-            D = DictionaryGenerator(n, N);
-            f = D * x;
-            r = 2 * norm(f);
-            % specify the random measurements to be used
-            m = (itr_i - 1) * step_m + min_m;
-            A = randn(m, n);
-            %% LP
+    parfor itr_i = 1:total_itr_number
+        %% Generate signal
+        % define the sparse vector x
+        supp = sort(randsample(N, s));
+        x = zeros(N, 1);
+        x(supp) = randn(s, 1);
+        % Generate dictionary
+        D = DictionaryGenerator(n, N);
+        f = D * x;
+        r = 2 * norm(f);
+        % specify the random measurements to be used
+        m = (itr_i - 1) * step_m + min_m;
+        A = randn(m, n);
+        %% LP
+        if (s == 20)
             temp_lp_time = tic;
             fLP_main = LP_main(D, A, f, r);
             time_lp(itr_i) = toc(temp_lp_time);
-            %% CP
+        end
+
+        %% CP
+        if (s == 20)
             temp_cp_time = tic;
             fCP_main = CP_main(D, A, f, r, r);
             time_cp(itr_i) = toc(temp_cp_time);
-            %% Adaptive CP
-            temp_acp_time = tic;
-            fACP_main = ACP_main(D, A, f, r, T);
-            time_acp(itr_i) = toc(temp_acp_time);
-            %% Compute error
-            err_lp = norm(fLP_main - f) / norm(f);
-            err_cp = norm(fCP_main - f) / norm(f);
-            err_acp = norm(fACP_main(:, end) - f) / norm(f);
-            error_lp(itr_i) = err_lp;
-            error_cp(itr_i) = err_cp;
-            error_acp(itr_i) = err_acp;
         end
 
-        FileName = [temp_name, num2str(mcr)];
-        save(FileName);
-        notif = [num2str(100 * mcr / max_mcr), '%', '  (iteration time: ', num2str(toc(itr_time)), ')'];
-        disp(notif);
+        %% Adaptive CP
+        temp_acp_time = tic;
+        fACP_main = ACP_main(D, A, f, r, T);
+        time_acp(itr_i) = toc(temp_acp_time);
+        %% Compute error
+        err_lp = norm(fLP_main - f) / norm(f);
+        err_cp = norm(fCP_main - f) / norm(f);
+        err_acp = norm(fACP_main(:, end) - f) / norm(f);
+        error_lp(itr_i) = err_lp;
+        error_cp(itr_i) = err_cp;
+        error_acp(itr_i) = err_acp;
     end
 
-    %% Compute data average
-    for mcr = 1:max_mcr
-        FileName = [temp_name, num2str(mcr)];
-        load(FileName)
-        simulaiton_result.temp.error_lp = simulaiton_result.temp.error_lp + error_lp;
-        simulaiton_result.temp.error_cp = simulaiton_result.temp.error_cp + error_cp;
-        simulaiton_result.temp.error_acp = simulaiton_result.temp.error_acp + error_acp;
-        simulaiton_result.temp.time_lp = simulaiton_result.temp.time_lp + time_lp;
-        simulaiton_result.temp.time_cp = simulaiton_result.temp.time_cp + time_cp;
-        simulaiton_result.temp.time_acp = simulaiton_result.temp.time_acp + time_acp;
-    end
-
-    simulaiton_result.temp.error_lp = simulaiton_result.temp.error_lp ./ max_mcr;
-    simulaiton_result.temp.error_cp = simulaiton_result.temp.error_cp ./ max_mcr;
-    simulaiton_result.temp.error_acp = simulaiton_result.temp.error_acp ./ max_mcr;
-    simulaiton_result.temp.time_lp = simulaiton_result.temp.time_lp ./ max_mcr;
-    simulaiton_result.temp.time_cp = simulaiton_result.temp.time_cp ./ max_mcr;
-    simulaiton_result.temp.time_acp = simulaiton_result.temp.time_acp ./ max_mcr;
-    %% remove temporary file and save
-    FileName = [temp_name, '*.mat'];
-    delete(FileName);
-    SimName = ['simulation_result', '_N=', num2str(N), '_n=', num2str(n), '_s=', num2str(s(sparsity_irt)), '_T=', num2str(T), '_montecarlo_itr=', num2str(max_mcr)];
-    save(SimName, 'simulaiton_result');
+    FileName = [temp_name, num2str(mcr)];
+    save(FileName);
+    notif = [num2str(100 * mcr / max_mcr), '%', '  (iteration time: ', num2str(toc(itr_time)), ')'];
+    disp(notif);
 end
+
+%% Compute data average
+for mcr = 1:max_mcr
+    FileName = [temp_name, num2str(mcr)];
+    load(FileName)
+    simulaiton_result.temp.error_lp = simulaiton_result.temp.error_lp + error_lp;
+    simulaiton_result.temp.error_cp = simulaiton_result.temp.error_cp + error_cp;
+    simulaiton_result.temp.error_acp = simulaiton_result.temp.error_acp + error_acp;
+    simulaiton_result.temp.time_lp = simulaiton_result.temp.time_lp + time_lp;
+    simulaiton_result.temp.time_cp = simulaiton_result.temp.time_cp + time_cp;
+    simulaiton_result.temp.time_acp = simulaiton_result.temp.time_acp + time_acp;
+end
+
+simulaiton_result.temp.error_lp = simulaiton_result.temp.error_lp ./ max_mcr;
+simulaiton_result.temp.error_cp = simulaiton_result.temp.error_cp ./ max_mcr;
+simulaiton_result.temp.error_acp = simulaiton_result.temp.error_acp ./ max_mcr;
+simulaiton_result.temp.time_lp = simulaiton_result.temp.time_lp ./ max_mcr;
+simulaiton_result.temp.time_cp = simulaiton_result.temp.time_cp ./ max_mcr;
+simulaiton_result.temp.time_acp = simulaiton_result.temp.time_acp ./ max_mcr;
+%% remove temporary file and save
+FileName = [temp_name, '*.mat'];
+delete(FileName);
+SimName = ['simulation_result', '_N=', num2str(N), '_n=', num2str(n), '_s=', num2str(s), '_T=', num2str(T), '_montecarlo_itr=', num2str(max_mcr)];
+save(SimName, 'simulaiton_result');
 
 %%
 disp('########################### H128B717 ###########################');
