7a23c1c7caa7c5562bdf87b6f24a0a1d0ff9e241
HosseinBeheshti <h.beheshti92@gmail.com>
2022-01-12 22:47:36 +0330

    next figure generation file added

--- adaptive_process_visualization.m
+++ adaptive_process_visualization.m
@@ -7,23 +7,22 @@ f_sim = [-3 -1.8];
 number_of_lines = 8;
 slope_cp = [0.37, 0.22, -0.86, -0.49, -0.80, 0.76, -0.21, 0.53];
 offset_cp = [1.37, -2.22, -1.86, -4.49, 5.80, -5.76, -7.21, 7.53];
-x = linspace(-10, 10);
+x = linspace(-9, 9);
 l_cp = zeros(length(slope_cp), length(x));
 Polyhedron_cp = zeros(4, 2);
 figure_cp = figure;
 hold on;
-plot(f_sim(1), f_sim(2), '.r', 'markersize', 10);
+plot(f_sim(1), f_sim(2), '.b', 'markersize', 10);
 
 for i = 1:number_of_lines
     l_cp(i, :) = slope_cp(i) * x + offset_cp(i);
 
     if i < 5
-        plot(x, l_cp(i, :), ':', 'LineWidth', 1.2);
+        plot(x, l_cp(i, :), ':', 'LineWidth', 2);
     else
-        plot(x, l_cp(i, :));
+        plot(x, l_cp(i, :), 'LineWidth', 2);
     end
 
-    pause(1);
 end
 
 Polyhedron_cp(1, :) = InterX([x; l_cp(1, :)], [x; l_cp(3, :)]);
@@ -32,31 +31,44 @@ Polyhedron_cp(3, :) = InterX([x; l_cp(2, :)], [x; l_cp(4, :)]);
 Polyhedron_cp(4, :) = InterX([x; l_cp(2, :)], [x; l_cp(3, :)]);
 pgon_cp = polyshape(Polyhedron_cp(:, 1), Polyhedron_cp(:, 2));
 plot(pgon_cp, 'FaceColor', 'red', 'FaceAlpha', 0.1);
+legend('$f$', 'L1', 'L2', 'L3', 'L4', 'L5', 'L6', 'L7', 'L8', 'Poly1', 'Interpreter', 'latex');
 hold off;
+saveas(gcf, 'CP.png')
 %% ACP
+pause(1);
 close all;
 slope_acp = [-0.80, 0.76, -1.81, 0.53, 0.53, -0.83, -2.25, 0.86];
 offset_acp = [5.80, -5.76, -12.21, 7.53, 0.31, -3.30, -9.43, 0.34];
-Polyhedron_acp = zeros(8, 2);
+Polyhedron_acp1 = zeros(4, 2);
+Polyhedron_acp2 = zeros(4, 2);
 figure2_acp = figure;
 hold on;
-plot(f_sim(1), f_sim(2), '.r', 'markersize', 10);
+plot(f_sim(1), f_sim(2), '.b', 'markersize', 10);
 l_acp = zeros(length(slope_acp), length(x));
 
 for i = 1:number_of_lines
     l_acp(i, :) = slope_acp(i) * x + offset_acp(i);
+
     if i < 5
-        plot(x, l_acp(i, :), ':', 'LineWidth', 1.2);
+        plot(x, l_acp(i, :), ':', 'LineWidth', 2);
     else
-        plot(x, l_acp(i, :));
+        plot(x, l_acp(i, :), 'LineWidth', 2);
     end
-    pause(1);
+
 end
 
-Polyhedron_acp(1, :) = InterX([x; l_acp(1, :)], [x; l_acp(3, :)]);
-Polyhedron_acp(2, :) = InterX([x; l_acp(1, :)], [x; l_acp(4, :)]);
-Polyhedron_acp(3, :) = InterX([x; l_acp(2, :)], [x; l_acp(4, :)]);
-Polyhedron_acp(4, :) = InterX([x; l_acp(2, :)], [x; l_acp(3, :)]);
-pgon_acp = polyshape(Polyhedron_acp(:, 1), Polyhedron_acp(:, 2));
-plot(pgon_acp, 'FaceColor', 'red', 'FaceAlpha', 0.1);
+Polyhedron_acp1(1, :) = InterX([x; l_acp(1, :)], [x; l_acp(2, :)]);
+Polyhedron_acp1(2, :) = InterX([x; l_acp(2, :)], [x; l_acp(3, :)]);
+Polyhedron_acp1(3, :) = InterX([x; l_acp(3, :)], [x; l_acp(4, :)]);
+Polyhedron_acp1(4, :) = InterX([x; l_acp(4, :)], [x; l_acp(1, :)]);
+pgon_acp1 = polyshape(Polyhedron_acp1(:, 1), Polyhedron_acp1(:, 2));
+plot(pgon_acp1, 'FaceColor', 'blue', 'FaceAlpha', 0.1);
+Polyhedron_acp2(1, :) = InterX([x; l_acp(5, :)], [x; l_acp(6, :)]);
+Polyhedron_acp2(2, :) = InterX([x; l_acp(5, :)], [x; l_acp(7, :)]);
+Polyhedron_acp2(4, :) = InterX([x; l_acp(6, :)], [x; l_acp(8, :)]);
+Polyhedron_acp2(3, :) = InterX([x; l_acp(7, :)], [x; l_acp(8, :)]);
+pgon_acp2 = polyshape(Polyhedron_acp2(:, 1), Polyhedron_acp2(:, 2));
+plot(pgon_acp2, 'FaceColor', 'green', 'FaceAlpha', 0.5);
+legend('$f$', 'L1', 'L2', 'L3', 'L4', 'L5', 'L6', 'L7', 'L8', 'Poly1', 'Poly2', 'Interpreter', 'latex');
 hold off;
+saveas(gcf, 'ACP.png')
