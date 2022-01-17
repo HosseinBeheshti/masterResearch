9d351fd8e9158567fa21eb5ed4d05d64fb6e6a5c
HosseinBeheshti <h.beheshti92@gmail.com>
2022-01-17 16:44:25 +0330

    change log

--- prepare_final_results.m
+++ prepare_final_results.m
@@ -4,25 +4,25 @@ close all;
 %% load data and prepare result packet
 % s = 20
 load('simulation_result_N=1000_n=50_s=20_T=10_montecarlo_itr=10');
-final_result_s20.error_lp = mean(simulaiton_result.error_lp);
-final_result_s20.error_cp = mean(simulaiton_result.error_cp);
-final_result_s20.error_acp = mean(simulaiton_result.error_acp);
+final_result_s20.error_lp = 10 * log10(mean(simulaiton_result.error_lp)));
+final_result_s20.error_cp = 10 * log10(mean(simulaiton_result.error_cp));
+final_result_s20.error_acp = 10 * log10(mean(simulaiton_result.error_acp));
 final_result_s20.time_lp = mean(simulaiton_result.time_lp);
 final_result_s20.time_cp = mean(simulaiton_result.time_cp);
 final_result_s20.time_acp = mean(simulaiton_result.time_acp);
 % s = 30
 load('simulation_result_N=1000_n=50_s=30_T=10_montecarlo_itr=10');
-final_result_s30.error_lp = mean(simulaiton_result.error_lp);
-final_result_s30.error_cp = mean(simulaiton_result.error_cp);
-final_result_s30.error_acp = mean(simulaiton_result.error_acp);
+final_result_s30.error_lp = 10 * log10(mean(simulaiton_result.error_lp));
+final_result_s30.error_cp = 10 * log10(mean(simulaiton_result.error_cp));
+final_result_s30.error_acp = 10 * log10(mean(simulaiton_result.error_acp));
 final_result_s30.time_lp = mean(simulaiton_result.time_lp);
 final_result_s30.time_cp = mean(simulaiton_result.time_cp);
 final_result_s30.time_acp = mean(simulaiton_result.time_acp);
 % s = 40
 load('simulation_result_N=1000_n=50_s=40_T=10_montecarlo_itr=10');
-final_result_s40.error_lp = mean(simulaiton_result.error_lp);
-final_result_s40.error_cp = mean(simulaiton_result.error_cp);
-final_result_s40.error_acp = mean(simulaiton_result.error_acp);
+final_result_s40.error_lp = 10 * log10(mean(simulaiton_result.error_lp));
+final_result_s40.error_cp = 10 * log10(mean(simulaiton_result.error_cp));
+final_result_s40.error_acp = 10 * log10(mean(simulaiton_result.error_acp));
 final_result_s40.time_lp = mean(simulaiton_result.time_lp);
 final_result_s40.time_cp = mean(simulaiton_result.time_cp);
 final_result_s40.time_acp = mean(simulaiton_result.time_acp);
